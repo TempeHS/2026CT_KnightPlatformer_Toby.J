60747d9a32a377f322c495e4bd137fcdac1a5a51
Toby J <toby.johnson19@education.nsw.gov.au>
2026-06-01 11:49:31 +1000

    Finished: basic Player Movement, Jumping and a basic placeholder arena with platforms for testing

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -35,7 +35,7 @@ public class PlayerMovement : MonoBehaviour
 
     private bool IsGrounded()
     {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        return Physics2D.OverlapCircle(groundCheck.position, 0.5f, groundLayer);
     }
 
     private void Flip()
