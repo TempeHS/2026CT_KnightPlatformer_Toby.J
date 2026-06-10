1b737aa09a37a8159a024a694c7b6d171021c1b0
Toby J <toby.johnson19@education.nsw.gov.au>
2026-06-11 09:27:33 +1000

    WIP: Working on healthbar GUI+ Added spikes, damaging, death, respawn

---
+++ Assets/GameController.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+    Vector2 startPos;
+    SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        startPos = transform.position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Obstacle"))
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        StartCoroutine(Respawn(0.5f));
+    }
+
+    IEnumerator Respawn(float duration)
+    {
+        spriteRenderer.enabled = false;
+        yield return new WaitForSeconds(duration);
+        transform.position = startPos;
+        spriteRenderer.enabled = true;
+    }
+}
