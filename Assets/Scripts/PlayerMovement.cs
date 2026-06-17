02a6ce3bafdf7842bd87effed4a0f62c8595bc1c
Toby J <toby.johnson19@education.nsw.gov.au>
2026-06-17 10:36:29 +1000

    WIP: Player Dash

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -6,25 +7,45 @@ public class PlayerMovement : MonoBehaviour
     private float speed = 8f;
     private float jumpingPower = 16f;
     private bool isFacingRight = true;
+    private bool isDashing = false;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
+    [SerializeField] float dashSpeed = 10f;
+    [SerializeField] float dashDuration = 0.15f;
+    [SerializeField] float dashCooldown = 1f;
+
 
     void Update()
     {
+        if(isDashing)
+        {
+            return;
+        }
         horizontal = Input.GetAxisRaw("Horizontal");
         if (Input.GetButtonDown("Jump") && IsGrounded())
-    {
+    
         rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
-    }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            StartCoroutine(Dash());
+        }
+    
+        
+    
+
 
 
         Flip();
     }
 
-    private void FixedUpdate()
+   private void FixedUpdate()
     {
+        if (isDashing)
+            return;
+
         rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
     }
 
@@ -43,4 +64,19 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = localScale;
         }
     }
+
+
+    private IEnumerator Dash()
+    {
+        isDashing = true;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;   
+        float direction = isFacingRight ? 1f : -1f;
+        rb.linearVelocity = new Vector2(direction * dashSpeed, 0f);
+        yield return new WaitForSeconds(dashDuration);
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+
+    }
+
 }
